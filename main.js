2fc449012f787cbbd15be9ce42d148c9aaa6df86
Sayed <sayemhasan110@gmail.com>
2022-11-11 10:32:48 +0000

    update items

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
 const express = require("express");
-var cors = require('cors')  
+var cors = require('cors')
 const app = express();
 
 app.use(cors())
 
 
 const items = [
-    "Item 1",
-    "Item 2",
-    "Item 3",
-    "Item 4",
-    "Item 5"
+    {
+        name: "Camera",
+        price: 19.25,
+        id: 1,
+        category: "Camera & Photo",
+    },
+    {
+        name: "Dell X5",
+        price: 450,
+        id: 2,
+        category: "Computer & Laptop",
+    },
+    {
+        name: "1L fresh milk",
+        price: 1.99,
+        id: 3,
+        category: "Dairy",
+    },
+    {
+        name: "Philadelphia Cheese",
+        price: 2.99,
+        id: 4,
+        category: "Dairy",
+    },
+    {
+        name: "Naissance Fractionated Coconut Oil",
+        price: 10.50,
+        id: 5,
+        category: "Oils",
+    }
 ]
 
 app.get("/", (request, response) => {
-   
+
     response.json(items)
 })
 
-const port  = 9300;
+const port = 9300;
 app.listen(port, () => {
     console.log("listening on port", port)
 });
